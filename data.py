8d9be9185732e0f3d5318f983118ed2efe6a7228
Manny Ramos <96265592+ramosman1@users.noreply.github.com>
2022-07-12 00:34:17 -0700

    Add files via upload

---
+++ data.py
@@ -0,0 +1,61 @@
+# pyt data here to use as test data, while database connection is set up
+mock_data = [
+    {
+        'id': 1,
+        'price': 30.00,
+        'stock': 50,
+        'title': 'tee',
+        'image': 'tee.jpg',
+        'category': 'apparel'
+    },
+    {
+        'id': 2,
+        'price': 40.00,
+        'stock': 50,
+        'title': 'shorts',
+        'image': 'shorts.jpg',
+        'category': 'shorts'
+    },
+    {
+        'id': 3,
+        'price': 50.00,
+        'stock': 50,
+        'title': 'sweater',
+        'image': 'sweater.jpg',
+        'category': 'sweater'
+    },
+    {
+        'id': 4,
+        'price': 60.00,
+        'stock': 50,
+        'title': 'sweats',
+        'image': 'sweats.jpg',
+        'category': 'sweats'
+    },
+    {
+        'id': 5,
+        'price': 45.00,
+        'stock': 50,
+        'title': 'briefs',
+        'image': 'briefs.jpg',
+        'category': 'underwear'
+    },
+    {
+        'id': 6,
+        'price': 35.00,
+        'stock': 50,
+        'title': 'beanie',
+        'image': 'skullcap.jpg',
+        'category': 'headwear'
+    },
+
+    {
+        'id': 7,
+        'price': 30.00,
+        'stock': 50,
+        'title': 'cap',
+        'image': 'cap.jpg',
+        'category': 'hats'
+    }
+
+]
